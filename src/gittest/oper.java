d753381fb2c7727e80d386ed5cdcd20f8e4fec24
changwoo LEE <ckddn1324@gmail.com>
2022-09-28 21:52:50 +0900

    Initial commit

---
+++ src/gittest/oper.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class oper {
+    public static void main(String[] args){
+
+        you in = new you("changwo");
+        System.out.println("Hello world!");
+
+        in.showInfo();
+    }
+}
---
+++ src/gittest/you.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class you {
+
+    public String name = "Lee";
+
+    you(String name){
+        this.name = name;
+    }
+    public void showInfo(){
+        System.out.println(name);
+    }
+}
